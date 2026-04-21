22f14ce244bea7722ebf025257e890ed49ce3486
Ramiz Soltanov <205435254+SolidusLeaf@users.noreply.github.com>
2026-04-21 15:16:13 +0500

    Add light sensor functionality to ex3

---
+++ src/ex3.cpp
@@ -0,0 +1,23 @@
+
+
+
+#include "Arduino.h"
+
+#define LIGHT_SENSOR_PIN 33
+
+/****************************************************/
+void setup(void) 
+{
+    Serial.begin(115200);
+    pinMode(LIGHT_SENSOR_PIN, INPUT);  // Light sensor (analog input)
+}
+
+
+/****************************************************/
+void loop(void) 
+{
+    int sensorValue = analogRead(LIGHT_SENSOR_PIN);  // Read the light sensor value
+    Serial.println(sensorValue);  // Print the sensor value
+    delay(500);  // Wait for 500 ms
+
+}
